b4d439d50ebbdcdbd797f389ae6bf740b26dce9b
Laveah Haneef <laveahwwghaneef@hotmail.com>
2026-08-19 17:15:18 +0200

    chore:group related constants and methods

--- constants.py
+++ constants.py
@@ -1,30 +1,23 @@
-class ErrorMessages:
-    FILE_NOT_FOUND = 'The specified file was not found.'
-    INVALID_INPUT = 'The input provided is invalid.'
-    CONNECTION_TIMEOUT = 'The connection has timed out.'
-    UNAUTHORIZED_ACCESS = 'Access is denied due to unauthorized credentials.'
-    SERVER_ERROR = 'The server encountered an internal error.'
+class Constants:
+    BASE_URL = 'https://api.example.com/'
+    TIMEOUT = 30
+    RETRY_LIMIT = 5
+    HEADERS = {'Content-Type': 'application/json'}
+    STATUS_CODES = {
+        'success': 200,
+        'not_found': 404,
+        'server_error': 500
+    }
 
-ERROR_CODES = {
-    404: ErrorMessages.FILE_NOT_FOUND,
-    400: ErrorMessages.INVALID_INPUT,
-    408: ErrorMessages.CONNECTION_TIMEOUT,
-    401: ErrorMessages.UNAUTHORIZED_ACCESS,
-    500: ErrorMessages.SERVER_ERROR,
-}
+    @classmethod
+    def get_status_message(cls, code):
+        messages = {
+            cls.STATUS_CODES['success']: 'Request was successful',
+            cls.STATUS_CODES['not_found']: 'Resource not found',
+            cls.STATUS_CODES['server_error']: 'Internal server error'
+        }
+        return messages.get(code, 'Unknown status code')
 
-class ErrorHandler:
-    @staticmethod
-    def get_error_message(code):
-        return ERROR_CODES.get(code, 'An unknown error occurred.')
-
-    @staticmethod
-    def raise_error(code):
-        message = ErrorHandler.get_error_message(code)
-        raise Exception(message)  
-
-# Example usage:
-# try:
-#     ErrorHandler.raise_error(404)
-# except Exception as e:
-#     print(e)
+    @classmethod
+    def is_valid_code(cls, code):
+        return code in cls.STATUS_CODES.values()
